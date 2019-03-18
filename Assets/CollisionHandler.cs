424bfc4eedfb202ac054cc8137760958805192b8
DESKTOP-MVL99S9\Abdul <arbanglee@yahoo.com>
2019-03-17 20:47:14 -0700

    Final submission

---
+++ Assets/CollisionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CollisionHandler : MonoBehaviour
+{
+	
+	[Tooltip("in seconds")][SerializeField] float levelLoadDelay = 1f;
+	[Tooltip("FX Prefab on player")][SerializeField] GameObject deathFX;
+	
+	void OnTriggerEnter(Collider other){
+
+		print("Player triggered with an object.");
+		StartDeathSequence();
+		deathFX.SetActive(true);
+		Invoke("ReloadScene", levelLoadDelay);
+	}
+
+	private void StartDeathSequence(){
+
+		print("Player dying");
+		SendMessage("OnPlayerDeath");
+
+	}
+
+	private void ReloadScene(){
+		SceneManager.LoadScene(1);
+	}
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+	[SerializeField] GameObject deathFX;
+	[SerializeField] Transform parent;
+	[SerializeField] int scorePerHit = 12;
+	[SerializeField] int hits = 4;
+
+	ScoreBoard scoreBoard;
+		
+    // Start is called before the first frame update
+    void Start()
+    {
+		Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+		boxCollider.isTrigger = false;
+
+		scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+ 
+	void OnParticleCollision(GameObject other){
+		scoreBoard.ScoreHit(scorePerHit);
+			hits--;
+			if (hits <= 0){
+				KillEnemy();
+			}
+	}
+
+	private void KillEnemy(){
+		GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+		fx.transform.parent = parent;
+		Destroy(gameObject);
+	}
+}
